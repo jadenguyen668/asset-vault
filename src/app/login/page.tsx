705767b374952c610df1a25875f82cd9c7ac4b53
chaunhun <chaunhun1@gmail.com>
2026-04-24 11:41:16 +0700

    fix: make login page dynamic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { AuthForm } from '@/components/auth/AuthForm';
 
+export const dynamic = 'force-dynamic';
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-bg">
